57a9c32fe5f4e38361b9b7269736bd7bc22fc209
wittenator <wittenator@yahoo.de>
2018-06-24 12:01:20 +0200

    Added stopword data and created interactive visualisation for the topic extraction

--- src/rpcserver/nlp/tdidf.py
+++ src/rpcserver/nlp/tdidf.py
@@ -4,10 +4,11 @@ import spacy
 from sklearn.feature_extraction.text import TfidfVectorizer
 import regex as re
 from langdetect import detect
-from sklearn.manifold import TSNE
 from sklearn.decomposition import TruncatedSVD
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.decomposition import NMF
 
 
 def loadProjects():
@@ -38,33 +39,44 @@ def lemmatize(text):
     nlp = spacy.load('de')
     return nlp(text)
 
+# compute the tfidf matrice of the given corpus
 def TfIdf(dict):
     start = time.time()
     tfidf = TfidfVectorizer(tokenizer=lemmatize)
-    tfs = tfidf.fit_transform(list(dict.values())[:90])
+    tfs = tfidf.fit_transform(list(dict.values())[:40])
     print('TFIDF execution time: ', time.time() - start)
     return tfs
 
-def kmeans(tfs):
+# cluster the data in its semantic space by topic
+def cluster(tfs, num_topics=10):
     start = time.time()
-    k = 50
-    km = KMeans(n_clusters=k, init='k-means++', max_iter=100, n_init=5)
-    km.fit(tfs)
-    print('K-Means execution time: ', time.time() - start)
+    km = KMeans(n_clusters=num_topics).fit(tfs)
+    print('Clustering execution time: ', time.time() - start)
     return km
 
-
-def tsne(tfs):
+# perform a dimension reduction which maximizes inter-class variance and minimizes intra-class variance in order to get clean clusters
+def dimReduction(tfs_reduced, clusters):
     start = time.time()
-    clusters = kmeans(tfs)
-    k = 50
+    tfs_2d = LinearDiscriminantAnalysis(n_components=2).fit(tfs_reduced, clusters.labels_).transform(tfs_reduced)
+    print('Dimension reduction execution time: ', time.time() - start)
+    return tfs_2d
+
+def clusterNumberHeuristic(tfs):
+    return (tfs.shape[0]*tfs.shape[1])//tfs.count_nonzero()
+
+def topicExtractionNMF(tfs):
+    return NMF().fit_transform(tfs)
+# plot the documents in the corpus clustered by topics as a scatterplot
+def show(tfs):
+    k = clusterNumberHeuristic(tfs)//3
     tfs_reduced = TruncatedSVD(n_components=k, random_state=0).fit_transform(tfs)
-    tfs_embedded = TSNE(n_components=2, perplexity=2).fit_transform(tfs_reduced)
-    print('TSNE execution time: ', time.time() - start)
-    fig = plt.figure()
-    ax = plt.axes()
-    plt.scatter(tfs_embedded[:, 0], tfs_embedded[:, 1], marker="x", c = clusters.labels_)
+    tfs_topics = topicExtractionNMF(tfs)
+    print(tfs_topics.shape)
+    clusters = cluster(tfs_reduced, num_topics=3)
+    tfs_2d = dimReduction(tfs_reduced, clusters)
+    print(tfs_2d.shape)
+    plt.scatter(tfs_2d[:, 0], tfs_2d[:, 1], marker="x", c = clusters.labels_)
     plt.show()
 
 
-tsne(TfIdf(cleanProjectTexts()))
+show(TfIdf(cleanProjectTexts()))

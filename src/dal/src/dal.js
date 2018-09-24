2f5c8848c7224a909d69fff879bc20df04598064
wittenator <wittenator@yahoo.de>
2018-09-24 16:03:50 +0200

    Created postgres microservice with inital sql structure

---
+++ src/dal/src/dal.js
@@ -0,0 +1,253 @@
+"use strict";
+
+const path = require('path')
+const fs = require('fs-extra')
+const hash = require('object-hash')
+
+const express = require('express')
+const helmet = require('helmet')
+const fs = require('fs-extra')
+const bodyParser = require('body-parser')
+const https = require('https')
+const path = require('path')
+const { Pool } = require('pg')
+const pool = new Pool()
+
+const config = require(path.join(__dirname, 'config/config.json'))
+
+const server = express()
+server.use(helmet())
+
+// Server setting
+const PORT = process.env.PORT || 8080
+
+// Register body-parser
+server.use(bodyParser.json())
+server.use(bodyParser.urlencoded({ extended: true }))
+
+// Configure router
+const router = express.Router();
+server.use('/', router);
+
+https.createServer({
+			    key: fs.readFileSync(path.join(__dirname, config.ssl.keyPath)),
+			    cert: fs.readFileSync(path.join(__dirname, config.ssl.crtPath))
+			}, server).listen(PORT, () => {
+			    console.log(`API Server Started On Port ${PORT}!`)
+			})
+
+
+// Routes
+router.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, config.frontend.htmlPath))
+})	
+
+router.get('/api/projects', (req, res) => {
+	console.log(dataloader.out.files['projects'])
+	Promise.resolve(dataloader.out.files['projects'])
+		.then((value) => {res.status(200).json(value)})
+	
+})
+
+router.get('/api/institutions', (req, res) => {
+	console.log(dataloader.out.files['institutions'])
+	Promise.resolve(dataloader.out.files['institutions'])
+		.then((value) => {res.status(200).json(value)})
+	
+})
+
+// exit strategy
+process.on('exit', (err) => {  
+    console.log(err);
+})
+
+
+class Dataloader {
+	constructor(config, secrets){
+		this.paths = config.data
+		this.googleMapsClient = require('@google/maps').createClient({
+  			key: secrets.geocoding_API_key,
+  			Promise: Promise
+			})
+	}
+
+	async _read(file) {
+		try {
+			const filePath = path.join(__dirname, this.paths.in[file])
+			if (this.paths.in[file].endsWith('csv')) {
+				return alasql.promise(`SELECT * FROM CSV("${path.join(__dirname, this.paths.in[file])}")`)
+			}
+			else if (this.paths.in[file].endsWith('json')){
+				return fs.readJson(filePath)
+			}
+			else {
+				throw `${file} is not in a supported file format!`
+			}
+		}
+		catch(reason)
+		{
+			console.log(reason)
+			process.exit()
+		}
+	}
+
+	async load() {
+		try {
+			// a subselect does not work yet in alasql. That's why this is a bit ugly
+			this.files = {}
+			this.files.in = {}
+			this.files.out = {}
+			for (const key in this.paths.in) {
+				this.files.in[key] = await this._read(key)
+			}
+
+			alasql.fn.pattern = (address) => {
+				return '%' + address.split('\n')
+				                    .map(topic => topic.trim())
+				                    .filter(line => line != '')
+				                    .slice(0,2)
+				                    .join('\n') + '%'
+			}
+
+			alasql.fn.clean = (list) => {
+				return ((Array.isArray(list))?list:list.split(',').map(elem => elem.trim())).filter(elem => elem != '' && elem != undefined)
+			}
+
+			this.files.out['projects'] = await alasql.promise(`
+			SELECT proj.project_id AS id, 
+				   FIRST(proj.institution_id) AS institution_id, 
+				   clean(ARRAY(DISTINCT countryCode.code)) AS region, 
+				   clean(ARRAY(DISTINCT institutions.institution_id)) AS cooperating_institutions, 
+				   FIRST(DISTINCT tax.subject_area) AS subject_area, 
+				   FIRST(DISTINCT tax.review_board) AS review_board, 
+				   FIRST(DISTINCT tax.research_area) AS research_area,
+				   'Anonym' AS applicant,
+				   'DFG' AS sponsor,
+				   clean(FIRST(proj.participating_subject_areas_full_string)) AS side_topics,
+				   'Anonym' AS project_leader,
+				   FIRST(proj.funding_start_year) AS start_date,
+				   FIRST(proj.funding_end_year) AS end_date,
+				   FIRST(proj.title) AS title,
+				   FIRST(proj.project_abstract) AS abstract,
+				   '' AS href,
+				   '1' AS synergy
+
+
+			FROM ? AS proj
+
+			-- Match the country codes to projects
+			LEFT JOIN ? AS countries
+			ON proj.project_id = countries.project_id
+			LEFT JOIN ? AS countryCode
+			ON countries.country = countryCode.country
+
+			-- Match participating people to projects (not sure if it matches completly!!)
+			LEFT JOIN ? AS projectsPeople
+			ON proj.project_id = projectsPeople.project_id_number
+			LEFT JOIN ? AS people
+			ON projectsPeople.person_id = people.person_id
+			LEFT JOIN ? as institutions
+			ON institutions.name LIKE pattern(people.address)
+
+			-- Match taxonomy
+			LEFT JOIN ? AS subj
+			ON proj.project_id = subj.project_id
+			LEFT JOIN ? AS tax
+			ON subj.subject_area = tax.subject_area
+
+			GROUP BY proj.project_id
+			ORDER BY proj.project_id
+			`, [this.files.in['projects'], 
+				this.files.in['projects-countries'], 
+				this.files.in['countryNames'], 
+				this.files.in['projects-people'], 
+				this.files.in['people'],
+				this.files.in['institutions'], 
+				this.files.in['projects-subjects'],
+				this.files.in['taxonomy']])
+			console.log(this.files.out['projects'])
+        	return this._save('projects', this.files.out['projects'])	
+		}
+		catch(reason) {
+			console.log(reason)
+		}
+	}
+
+	async transform() {
+		const filePath = path.join(__dirname, this.paths.out['institutions'])
+		try {
+			if ((await fs.pathExists(filePath)))
+			{
+				const filetemp = await fs.readJson(filePath)
+				this.files.out['institutions'] = filetemp['data']
+				const hashvalue = hash(this.files.in['institutions']) + hash(this.files.out['projects'])
+				if ( hashvalue != filetemp['hash'] ) {
+					return this._geocodeInstitutions()
+				}
+				else {
+					console.log('Loading saved file!')
+				}
+			}
+			else {
+				console.log('Not found')
+				return this._geocodeInstitutions()
+			}
+		}
+		catch(reason) {
+			console.log(reason)
+		}
+	}
+
+	async _geocodeInstitutions() {
+
+		const _geocode =  async loc => {
+		return this.googleMapsClient.geocode({address: loc})
+				  .asPromise()
+				  .then((response) => {
+				  	console.log(loc, response.json.results[0]['geometry']['location'], '\n')
+				    return response.json.results[0]['geometry']['location']
+				  })
+				  .catch((err) => {
+				    console.log(err)
+				    return {lat: 'error', lng: 'error'}
+				  })
+		}	
+
+		try {
+			this.files.out['institutions'] = await alasql.promise(`
+				SELECT DISTINCT inst.*
+				FROM ? AS inst
+ 				JOIN ? AS proj
+				ON proj.institution_id = inst.institution_id
+				`,[this.files.in['institutions'], this.files.out['projects']])
+			for (var i in this.files.out['institutions']) {
+				this.files.out['institutions'][i]['loc'] = await _geocode(this.files.out['institutions'][i]['address'])
+			}
+			console.log(this.files.out['institutions'])
+			return this._save('institutions', {hash: hash(this.files.in['institutions']) + hash(this.files.out['projects']), data: this.files.out['institutions']})
+		}
+		catch (reason) {
+			console.log(reason)
+		}
+
+	}
+
+
+	print() {
+		if (this.file !== {}) {
+			console.log(this.file)
+		}
+	}
+
+	async _save(file, data) {
+		try {
+			return fs.writeJson(path.join(__dirname, this.paths.out[file]), data)
+			console.log('success!')
+		} catch (err) {
+			console.error(err)
+		}
+	}
+
+}
+
+module.exports = Dataloader
--- src/postgres/init.sql
+++ src/postgres/init.sql
@@ -1,10 +1,64 @@
 CREATE TABLE IF NOT EXISTS institutions (
-  institution_id INTEGER NOT NULL,
+  id INTEGER PRIMARY KEY ,
+  name TEXT,
+  address TEXT,
+  phone TEXT,
+  fax TEXT,
+  email TEXT,
+  internet TEXT
+);
+
+CREATE TABLE IF NOT EXISTS people (
+  id INTEGER NOT NULL PRIMARY KEY ,
   name TEXT,
   address TEXT,
   phone TEXT,
   fax TEXT,
   email TEXT,
   internet TEXT,
-  PRIMARY KEY(institution_id)
+  institution_name TEXT,
+  institution_id INTEGER REFERENCES institutions(id)
 );
+
+CREATE TABLE IF NOT EXISTS projects (
+  id INTEGER NOT NULL PRIMARY KEY ,
+  title TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS subjects (
+  subject_area TEXT NOT NULL PRIMARY KEY ,
+  review_board TEXT NOT NULL,
+  research_area TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS countryCode (
+  country TEXT NOT NULL PRIMARY KEY ,
+  code TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS projectsCountries (
+  project_id INTEGER REFERENCES projects(id),
+  country TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS projectsCountries (
+  project_id INTEGER REFERENCES projects(id),
+  person_id INTEGER REFERENCES people(id),
+  relation_type TEXT
+);
+
+CREATE TABLE IF NOT EXISTS projectsSubjects (
+  project_id INTEGER REFERENCES projects(id),
+  subject_area TEXT REFERENCES subjects(subject_area),
+  relation_type TEXT
+);
+
+CREATE TABLE IF NOT EXISTS institutionsGeolocations (
+  institution_id INTEGER REFERENCES institutions(id),
+  lat FLOAT8,
+  long FLOAT8
+);
+
+
+
+

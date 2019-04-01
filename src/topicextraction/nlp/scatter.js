fedf4bec33c2154ebc553a6e0a84e08e38904e53
wittenator <wittenator@yahoo.de>
2019-04-01 18:58:20 +0200

    Added cluster topography

--- src/topicextraction/nlp/scatter.js
+++ src/topicextraction/nlp/scatter.js
@@ -1,11 +1,11 @@
-require.undef('circles');
+require.undef('scatter');
 
-define('circles', ['d3'], function (d3) {
+define('scatter', ['d3'], function (d3) {
 
     function draw(container, data, width, height) {
       var margin ={top: (0.05*width), right: (0.1*width), bottom: (0.1*width), left: (0.05*width)};
-      width = 0.8*width;
-      height = 0.9*height;
+      //width = 0.8*width;
+      //height = 0.9*height;
 
       ///////// set up svg and clipping
 
@@ -23,34 +23,6 @@ define('circles', ['d3'], function (d3) {
         .attr("width", width)
         .attr("height", height);
 
-      function zoomed() {
-      // create new scale ojects based on event
-          var new_x = d3.event.transform.rescaleX(x);
-          var new_y = d3.event.transform.rescaleY(y);
-      // update axes
-          gX.call(xAxis.scale(new_x));
-          gY.call(yAxis.scale(new_y));
-          console.log(points)
-          svg.selectAll(".dot")
-           .data(data.project_data)
-           .attr('cx', function(d) {return new_x(d.embpoint[0])})
-           .attr('cy', function(d) {return new_y(d.embpoint[1])});
-      }
-
-      // Pan and zoom
-      var zoom = d3.zoom()
-          .scaleExtent([.5, 20])
-          .extent([[0, 0], [width, height]])
-          .on("zoom", zoomed);
-
-      svg.append("rect")
-          .attr("width", width)
-          .attr("height", height)
-          .style("fill", "none")
-          .style("pointer-events", "all")
-          .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
-          .call(zoom);
-
       ///////// set up axes
 
       var x = d3.scaleLinear()
@@ -61,30 +33,15 @@ define('circles', ['d3'], function (d3) {
 
       var color = d3.scaleOrdinal(d3.schemeCategory10);
 
-      var xAxis = d3.axisBottom(x);
 
-      var yAxis = d3.axisLeft(y);
-
-      x.domain(d3.extent(data.project_data, function(d) { return d.embpoint[0]; })).nice();
-      y.domain(d3.extent(data.project_data, function(d) { return d.embpoint[1]; })).nice();
-
-      var gX = svg.append("g")
-          .attr("class", "x axis")
-          .attr("transform", "translate(0," + height + ")")
-          .call(xAxis);
-
-      var gY = svg.append("g")
-          .attr("class", "y axis")
-          .call(yAxis)
+      x.domain(d3.extent(data.project_data, function(d) { return d.embpoint[0]; }));
+      y.domain(d3.extent(data.project_data, function(d) { return d.embpoint[1]; }));
 
       ///////// insert data points and contour plot
-
-      var contours = d3.contourDensity()
-        .x(d => x(d.embpoint[0]))
-        .y(d => y(d.embpoint[1]))
+      var contours = d3.contours()
         .size([width, height])
-        .bandwidth(40)
-      (data.project_data)
+      (data.cluster_topography)
+
 
       svg.append("g")
           .attr("fill", "none")
@@ -98,7 +55,7 @@ define('circles', ['d3'], function (d3) {
           .attr("d", d3.geoPath());
 
       svg.selectAll(".dot")
-        .data(data.project_data);
+        .data(data.project_data)
         .enter().append("circle")
         .attr("class", "dot")
         .attr("r", 3.5)
@@ -127,6 +84,7 @@ define('circles', ['d3'], function (d3) {
           .attr("dy", ".35em")
           .style("text-anchor", "end")
           .text(function(d) { return d; });
+          
     }
     return draw;
 });

59855f55ccc82d568a62bd3c63f5b0bcb32488be
wittenator <wittenator@yahoo.de>
2019-04-03 13:12:15 +0200

    Added top words in tooltips

--- src/topicextraction/nlp/scatter.js
+++ src/topicextraction/nlp/scatter.js
@@ -78,7 +78,7 @@ define('scatter', ['d3'], function (d3) {
             div.transition()    
                 .duration(200)    
                 .style("opacity", .9);    
-            div .html(d.id + "<br/>"  + d.title)  
+            div .html(d.id + "<br/>"  + d.title + "<br/>"  + d.words)  
                 .style("left", (d3.event.pageX) + "px")   
                 .style("top", (d3.event.pageY - 28) + "px");  
             })          
